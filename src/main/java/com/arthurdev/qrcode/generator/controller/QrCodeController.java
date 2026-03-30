ef5d7196973094ca2be1048a06728d459f2dbd67
Arthur Marques <ahurvilasboas@gmail.com>
2026-03-30 18:24:43 -0300

    Criação do Service de geração do QRCODE

--- src/main/java/com/arthurdev/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/arthurdev/qrcode/generator/controller/QrCodeController.java
@@ -3,6 +3,7 @@ package com.arthurdev.qrcode.generator.controller;
 
 import com.arthurdev.qrcode.generator.dto.QrCodeGenerateRequest;
 import com.arthurdev.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.arthurdev.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
+
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+        try {
+            QrCodeGenerateResponse response =
+                    this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
